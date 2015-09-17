871c02afcb1920601ba3a57d08775e430c5951cd
ffmm <masamail4210@gmail.com>
2015-09-17 19:02:15 +0900

    ９章

---
+++ app/src/main/java/com/example/fm/MapRefuge/Refuge.java
@@ -0,0 +1,52 @@
+package com.example.fm.MapRefuge;
+
+public class Refuge {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private boolean near;
+    private int distance;
+
+    public Refuge(String name, String address, double latitude, double longitude) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.near = false;
+        this.distance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getLat() {
+        return latitude;
+    }
+
+    public double getLng() {
+        return longitude;
+    }
+
+    public boolean isNear() {
+        return near;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setNear(boolean near) {
+        this.near = near;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = (int) distance;
+    }
+
+}
